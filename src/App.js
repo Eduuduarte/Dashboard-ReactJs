b153fecdbeb973b204aa5893274a82357b623f64
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-08-06 13:33:23 -0300

    Bluild the sidebar

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { Navbar, Footer, Sidebar, ThemeSettings} from './components'
 import { ECommerce, Line, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor} from './pages';
 
+import { useStateContext } from "./contexts/ContextProvider";
+
 import './App.css';
 
 const App = () => {
-
-    const activeMenu = true;
+    const { activeMenu } = useStateContext();
 
     return (
         <div>
